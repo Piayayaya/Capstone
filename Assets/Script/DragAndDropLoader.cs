aab40fc27f56e3818f11f8c279f6c9bd0ee7c2ac
Heaven Fajardo <heavenfajardo82@gmail.com>
2025-10-22 11:59:51 +0800

    updated

---
+++ Assets/Script/DragAndDropLoader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DragAndDropLoader : MonoBehaviour
+{
+    // Scene name must match the asset exactly (case-sensitive on some platforms)
+    [SerializeField] private string sceneName = "DragAndDrop";
+
+    // Hook this to the Button → On Click()
+    public void LoadDragAndDrop()
+    {
+        Debug.Log("[DragAndDropLoader] Loading: " + sceneName);
+        SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+    }
+}
